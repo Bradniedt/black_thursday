aad359cc801609e7c6e262afa78db5d16a28fee0
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-28 16:09:08 -0600

    test passes for quantity

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,10 +1,12 @@
 class InvoiceItem
   attr_reader     :id,
                   :item_id,
-                  :invoice_id
+                  :invoice_id,
+                  :quantity
   def initialize(invoice_data)
-    @id = invoice_data[:id]
-    @item_id = invoice_data[:item_id]
+    @id         = invoice_data[:id]
+    @item_id    = invoice_data[:item_id]
     @invoice_id = invoice_data[:invoice_id]
+    @quantity   = invoice_data[:quantitiy]
   end
 end
