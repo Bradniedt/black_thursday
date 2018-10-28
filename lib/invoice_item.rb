ff2762a4fc17244a85211b42a18443fbc8f3eb8d
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-28 15:54:28 -0600

    created setup and test for invoice_item

---
+++ lib/invoice_item.rb
@@ -0,0 +1,6 @@
+class InvoiceItem
+
+  def initialize(invoice_data)
+    @id = invoice_data
+  end
+end
---
+++ test/invoice_item_test.rb
@@ -0,0 +1,20 @@
+require_relative './helper'
+require_relative '../lib/invoice_item'
+
+class InvoiceItemTest < Minitest::Test
+  def setup
+    @invoice_item = InvoiceItem.new({
+                    :id => 6,
+                    :item_id => 7,
+                    :invoice_id => 8,
+                    :quantity => 1,
+                    :unit_price => BigDecimal.new(10.99, 4),
+                    :created_at => Time.now,
+                    :updated_at => Time.now
+                                    })
+  end
+
+  def test_it_exists
+    assert_instance_of InvoiceItem, @invoice_item
+  end
+end
