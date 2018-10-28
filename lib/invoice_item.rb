7c5e7e44e4b79ecf87e0745bdde5d007d68c29c6
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-28 16:00:46 -0600

    update to invoice_id test, passes

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,8 +1,10 @@
 class InvoiceItem
   attr_reader     :id,
-                  :item_id
+                  :item_id,
+                  :invoice_id
   def initialize(invoice_data)
     @id = invoice_data[:id]
     @item_id = invoice_data[:item_id]
+    @invoice_id = invoice_data[:invoice_id]
   end
 end
