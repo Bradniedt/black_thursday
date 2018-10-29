cd9f3991f08863868a677a73a20b00d1c30022dd
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-28 22:48:29 -0600

    update to cc expiration date test and method

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,13 @@
 class Transaction
+  attr_accessor :credit_card_number,
+                :credit_card_expiration_date
   attr_reader   :id,
                 :invoice_id
   def initialize(tran_data)
     @id         = tran_data[:id]
     @invoice_id = tran_data[:invoice_id]
+    @credit_card_number = tran_data[:credit_card_number]
+    @credit_card_expiration_date = tran_data[:credit_card_expiration_date]
+    
   end
 end
