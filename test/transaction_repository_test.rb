3ce9f2fa8987f19c43635c5e20a38818609214fc
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-29 14:06:41 -0600

    additional update for houndci

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -49,11 +49,11 @@ class TransactionRepositoryTest < Minitest::Test
   end
 
   def test_it_can_update_an_transaction
-    @transaction.update(1,  {
+    @transaction.update(1, {
                   credit_card_number: '5757575757575757',
                   credit_card_expiration_date: '0121',
                   result: 'failed'
-                             } )
+                            } )
     assert_equal 5757575757575757, @transaction.find_by_id(1).credit_card_number
     assert_equal '0121', @transaction.find_by_id(1).credit_card_expiration_date
     assert_equal 'failed', @transaction.find_by_id(1).result
