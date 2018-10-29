e7864f809f6cfbcbbdbe5481f076ce000ac88c1d
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-29 14:04:27 -0600

    update to syntax via hound ci

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -5,11 +5,11 @@ require_relative '../lib/transaction_repository'
 class TransactionRepositoryTest < Minitest::Test
   def setup
     se = SalesEngine.from_csv( {
-        :items          => './data/items.csv',
-        :merchants      => './data/merchants.csv',
-        :invoices       => './data/invoices.csv',
-        :invoice_items  => './data/invoice_items.csv',
-        :transactions   => './data/transactions.csv'
+                                :items          => './data/items.csv',
+                                :merchants      => './data/merchants.csv',
+                                :invoices       => './data/invoices.csv',
+                                :invoice_items  => './data/invoice_items.csv',
+                                :transactions   => './data/transactions.csv'
                                 } )
     @transaction = se.transactions
   end
@@ -49,11 +49,11 @@ class TransactionRepositoryTest < Minitest::Test
   end
 
   def test_it_can_update_an_transaction
-    @transaction.update(1, {
+    @transaction.update(1,  {
                   credit_card_number: '5757575757575757',
                   credit_card_expiration_date: '0121',
                   result: 'failed'
-                      })
+                             } )
     assert_equal 5757575757575757, @transaction.find_by_id(1).credit_card_number
     assert_equal '0121', @transaction.find_by_id(1).credit_card_expiration_date
     assert_equal 'failed', @transaction.find_by_id(1).result
