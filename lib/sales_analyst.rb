4a6afcba9fb37bc97bf77073f272032d00501522
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-31 18:42:54 -0600

    testing for revenue by date

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -2,6 +2,7 @@ require_relative '../lib/sales_engine'
 require 'bigdecimal'
 require 'mathn'
 require 'date'
+require 'time'
 class SalesAnalyst
   attr_reader     :items, :merchants, :invoices, :transactions, :invoice_items, :customers
   def initialize(items, merchants, invoices, transactions, invoice_items, customers )
@@ -240,6 +241,29 @@ class SalesAnalyst
       final_total += total
     end
     BigDecimal.new(final_total, "#{final_total}".length - 1)
+  end
+
+  def all_successful_transactions
+    @transactions.all.find_all do |transaction|
+      transaction.result == :success
+    end
+  end
+
+  def successful_dates(date)
+    all_invoices = all_successful_transactions.find_all do |transaction|
+      @invoices.find_by_id(transaction.invoice_id)
+    end
+    all_invoices.find_all do |invoice|
+      invoice.created_at.to_s.slice(0..9) == date.to_s.slice(0..9)
+    end
+    # total_revenue = 0
+    # all_invoices = by_date.map do |transaction|
+    #   @invoices.find_by_id(transaction.invoice_id)
+    # end
+    # all_invoices.each do |invoice|
+    #   total_revenue += invoice_total(invoice.id)
+    # end
+    # total_revenue
   end
 
   # def top_merchant_for_customer(customer_id)
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -1,4 +1,5 @@
 require_relative './helper'
+require 'time'
 require_relative '../lib/sales_engine'
 class SalesAnalystTest < Minitest::Test
   def setup
@@ -12,90 +13,96 @@ class SalesAnalystTest < Minitest::Test
                                 } )
   end
 
-  def test_it_exists
-    assert_instance_of SalesAnalyst, @se.analyst
-  end
+  # def test_it_exists
+  #   assert_instance_of SalesAnalyst, @se.analyst
+  # end
+  #
+  # def test_it_can_return_average_items_per_merchant
+  #   assert_equal 2.88, @se.analyst.average_items_per_merchant
+  # end
+  #
+  # def test_it_can_count_total_number_of_items
+  #   assert_equal 1367.00, @se.analyst.count_all_items
+  # end
+  #
+  # def test_it_can_count_all_merchants
+  #   assert_equal 475.00, @se.analyst.count_all_merchants
+  # end
+  #
+  # def test_it_can_calculate_standard_deviation
+  #   assert_equal 3.26, @se.analyst.average_items_per_merchant_standard_deviation
+  # end
+  #
+  # def test_it_can_return_merchants_with_high_count
+  #   assert_equal 52, @se.analyst.merchants_with_high_item_count.count
+  # end
+  #
+  # def test_it_can_return_average_item_price_for_a_merchant
+  #   assert_equal 16.66, @se.analyst.average_item_price_for_merchant(12334105)
+  # end
+  #
+  # def test_it_can_return_average_price_for_all_merchants
+  #   assert_equal 350.29, @se.analyst.average_average_price_per_merchant
+  # end
+  #
+  # def test_can_record_standard_deviation_for_item_price
+  #   assert_equal 2899.93, @se.analyst.price_standard_deviation
+  # end
+  #
+  # def test_it_can_return_golden_items
+  #   assert_equal 5, @se.analyst.golden_items.length
+  # end
+  #
+  # def test_it_can_find_average_invoices_per_merchant
+  #   assert_equal 10.49, @se.analyst.average_invoices_per_merchant
+  # end
+  #
+  # def test_it_can_return_invoices_per_merchant_standard_deviation
+  #   assert_equal 3.29, @se.analyst.average_invoices_per_merchant_standard_deviation
+  # end
+  #
+  # def test_it_can_return_top_merchants_by_invoice_count
+  #   assert_equal 12, @se.analyst.top_merchants_by_invoice_count.count
+  # end
+  #
+  # def test_it_can_return_bottom_merchants_by_invoice_count
+  #   assert_equal 4, @se.analyst.bottom_merchants_by_invoice_count.count
+  # end
+  #
+  # def test_it_can_convert_date_to_days
+  #   assert_equal 'Monday', @se.analyst.date_to_days('2018-10-29')
+  # end
+  #
+  # def test_it_can_find_average_day_value
+  #   assert_equal 712.14, @se.analyst.average_days
+  # end
+  #
+  # def test_it_can_find_standard_deviation_for_invoice_created_at
+  #   assert_equal 18.07, @se.analyst.invoice_days_standard_deviation
+  # end
+  #
+  # def test_it_can_count_top_days_by_invoice
+  #   assert_equal ['Wednesday'], @se.analyst.top_days_by_invoice_count
+  # end
+  #
+  # def test_it_can_calculate_percentage_of_all_statuses
+  #   assert_equal 29.55, @se.analyst.invoice_status(:pending)
+  #   assert_equal 56.95, @se.analyst.invoice_status(:shipped)
+  #   assert_equal 13.5, @se.analyst.invoice_status(:returned)
+  # end
+  #
+  # def test_can_find_if_invoice_is_paid_in_full
+  #   assert_equal true, @se.analyst.invoice_paid_in_full?(2179)
+  # end
+  #
+  # def test_it_can_return_an_invoice_total
+  #   assert_equal 31075.11, @se.analyst.invoice_total(2179)
+  # end
 
-  def test_it_can_return_average_items_per_merchant
-    assert_equal 2.88, @se.analyst.average_items_per_merchant
-  end
-
-  def test_it_can_count_total_number_of_items
-    assert_equal 1367.00, @se.analyst.count_all_items
-  end
-
-  def test_it_can_count_all_merchants
-    assert_equal 475.00, @se.analyst.count_all_merchants
-  end
-
-  def test_it_can_calculate_standard_deviation
-    assert_equal 3.26, @se.analyst.average_items_per_merchant_standard_deviation
-  end
-
-  def test_it_can_return_merchants_with_high_count
-    assert_equal 52, @se.analyst.merchants_with_high_item_count.count
-  end
-
-  def test_it_can_return_average_item_price_for_a_merchant
-    assert_equal 16.66, @se.analyst.average_item_price_for_merchant(12334105)
-  end
-
-  def test_it_can_return_average_price_for_all_merchants
-    assert_equal 350.29, @se.analyst.average_average_price_per_merchant
-  end
-
-  def test_can_record_standard_deviation_for_item_price
-    assert_equal 2899.93, @se.analyst.price_standard_deviation
-  end
-
-  def test_it_can_return_golden_items
-    assert_equal 5, @se.analyst.golden_items.length
-  end
-
-  def test_it_can_find_average_invoices_per_merchant
-    assert_equal 10.49, @se.analyst.average_invoices_per_merchant
-  end
-
-  def test_it_can_return_invoices_per_merchant_standard_deviation
-    assert_equal 3.29, @se.analyst.average_invoices_per_merchant_standard_deviation
-  end
-
-  def test_it_can_return_top_merchants_by_invoice_count
-    assert_equal 12, @se.analyst.top_merchants_by_invoice_count.count
-  end
-
-  def test_it_can_return_bottom_merchants_by_invoice_count
-    assert_equal 4, @se.analyst.bottom_merchants_by_invoice_count.count
-  end
-
-  def test_it_can_convert_date_to_days
-    assert_equal 'Monday', @se.analyst.date_to_days('2018-10-29')
-  end
-
-  def test_it_can_find_average_day_value
-    assert_equal 712.14, @se.analyst.average_days
-  end
-
-  def test_it_can_find_standard_deviation_for_invoice_created_at
-    assert_equal 18.07, @se.analyst.invoice_days_standard_deviation
-  end
-
-  def test_it_can_count_top_days_by_invoice
-    assert_equal ['Wednesday'], @se.analyst.top_days_by_invoice_count
-  end
-
-  def test_it_can_calculate_percentage_of_all_statuses
-    assert_equal 29.55, @se.analyst.invoice_status(:pending)
-    assert_equal 56.95, @se.analyst.invoice_status(:shipped)
-    assert_equal 13.5, @se.analyst.invoice_status(:returned)
-  end
-
-  def test_can_find_if_invoice_is_paid_in_full
-    assert_equal true, @se.analyst.invoice_paid_in_full?(2179)
-  end
-
-  def test_it_can_return_an_invoice_total
-    assert_equal 31075.11, @se.analyst.invoice_total(2179)
+  def test_it_can_find_total_revenue_by_date
+    assert_equal 4158, @se.analyst.all_successful_transactions.count
+    date = Time.parse("2009-02-07")
+    assert_equal 1000, @se.analyst.successful_dates(date).count
   end
 
   # def test_it_can_separate_sucessful_transactions_by_customer
